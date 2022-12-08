5a91502fe6e49c50ac4840e0b2ecff05b7b60a7d
seo0j <seo0jjjjj@gmail.com>
2022-12-09 02:47:34 +0900

    prepareFianl

--- src/swing/Main.java
+++ src/swing/Main.java
@@ -1,7 +1,53 @@
 package swing;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello World!");
+       new LayoutTest();
     }
+}
+
+class LayoutTest extends JFrame implements ActionListener {
+    int a;
+    JLabel count;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("내가 객체!");
+    }
+
+    class Add implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Button pushed");
+
+        }
+    }
+    public LayoutTest(){
+        setLayout(new FlowLayout());
+       a=0;
+       setVisible(true);
+       setSize(400,150);
+       add(new JLabel("현재의 카운터 값: "));
+       count = new JLabel("0");
+        add(count);
+       JButton jButton = new JButton();
+       jButton.setSize(20,20);
+       add(jButton);
+       jButton.addActionListener(new ActionListener(){
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               System.out.println("하하하하");
+           }
+       });
+        System.out.println(this.getLayout().toString());
+
+    }
+
+
 }
